6915637f4d890ade9f57cf3acc42d91ffb6de41e
nanse0 <rlasktp1@naver.com>
2021-06-09 23:07:18 +0900

    카카오맵 근처 음식점 마커 띄우기 작업중 i/system.out (httplog)-static issbsettingenabled false 오류로 진행 막힘

---
+++ app/src/main/java/com/example/ossw5team/SearchResult.java
@@ -0,0 +1,31 @@
+package com.example.ossw5team;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class SearchResult {
+    @SerializedName("meta")
+    @Expose
+    private Meta meta;
+    @SerializedName("documents")
+    @Expose
+    private List<Document> documents = null;
+
+    public Meta getMeta() {
+        return meta;
+    }
+
+    public void setMeta(Meta meta) {
+        this.meta = meta;
+    }
+
+    public List<Document> getDocuments() {
+        return documents;
+    }
+
+    public void setDocuments(List<Document> documents) {
+        this.documents = documents;
+    }
+}
